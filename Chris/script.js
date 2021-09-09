60d6423f9b9ac81d37a3e3ffdff93bda0edaf3be
chris.suo <chris.suo@quantatw.com>
2021-09-09 18:49:41 +0800

    dynamic data display

---
+++ Chris/script.js
@@ -0,0 +1,50 @@
+var data = [
+    {
+        "imgSrc":"res/pic1.jpg",
+        "meta":"2021/08/15",
+        "postTitle":"Steak is good good eat!",
+        "postContent":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero dicta dolore molestias ipsam inventore nihil quos doloribus atque repudiandae fugiat. Quibusdam aut incidunt aliquam doloremque expedita inventore dolorum vitae culpa?"
+    },{
+        "imgSrc":"res/pic2.jpg",
+        "meta":"2021/08/15",
+        "postTitle":"I'm lovin' it!",
+        "postContent":"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut illo tenetur qui sit, iste incidunt atque quas, ab error sunt laborum deserunt, excepturi expedita repudiandae dolores. Debitis alias tempore soluta?"
+    }
+];
+
+let postsDiv = document.getElementById('posts');
+
+data.forEach(element => {
+    let post = document.createElement('div');    
+    post.classList.add('post');
+    let img = document.createElement('img');
+    img.classList.add('postImg');
+    img.src = element.imgSrc;
+    img.alt="";
+    post.appendChild(img);
+
+    let postData = document.createElement('div');
+    postData.classList.add('postData');
+    let meta = document.createElement('h5');
+    meta.classList.add('meta');
+    meta.innerHTML = element.meta;
+    postData.appendChild(meta);
+
+    let postTitle = document.createElement('h3');
+    postTitle.classList.add('postTitle');
+    postTitle.innerHTML = element.postTitle;
+    postData.appendChild(postTitle);
+
+    let postContent = document.createElement('p');
+    postContent.classList.add('postContent');
+    postContent.innerHTML = element.postContent;
+    postData.appendChild(postContent);
+
+    let btn = document.createElement('input');
+    btn.type = "button";
+    btn.value = "Read More";
+    postData.appendChild(btn);
+    
+    post.appendChild(postData);
+    postsDiv.appendChild(post);
+});
