a112dd77b3f9ff3ddfe7c394f0e70e6255bd2a8d
clazenchang <clazenchang@gmail.com>
2022-01-16 19:32:30 +0800

    20220116

--- Clazen/project/script.js
+++ Clazen/project/script.js
@@ -1,4 +1,19 @@
 
+let bar_toggle = 0;
+document.getElementById("bar_svg").addEventListener('click', () => {
+    if(bar_toggle){
+        document.getElementById("bar_menu").classList.add("d-none");
+    }
+    else{
+        document.getElementById("bar_menu").classList.remove("d-none");
+    }
+
+    bar_toggle = !bar_toggle;
+});
+
+
+
+
 let ref_num = 1;
 document.getElementById('change_bg_btn').addEventListener('click', ()=>
 {
@@ -11,4 +26,11 @@ document.getElementById('change_bg_btn').addEventListener('click', ()=>
     ref_num = !ref_num ;    
 }
 );
+
+
+
+
+
+
+
 
