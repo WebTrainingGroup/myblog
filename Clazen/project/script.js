daac2d89f9ba8bdc60d1f35186d3a299c43c8ee6
clazenchang <clazenchang@gmail.com>
2022-03-27 14:21:32 +0800

    20220327-1

--- Clazen/project/script.js
+++ Clazen/project/script.js
@@ -1,10 +1,10 @@
 
 let bar_toggle = 0;
 document.getElementById("bar_svg").addEventListener('click', () => {
-    if(bar_toggle){
+    if (bar_toggle) {
         document.getElementById("bar_menu").classList.add("d-none");
     }
-    else{
+    else {
         document.getElementById("bar_menu").classList.remove("d-none");
     }
 
@@ -15,15 +15,14 @@ document.getElementById("bar_svg").addEventListener('click', () => {
 
 
 let ref_num = 1;
-document.getElementById('change_bg_btn').addEventListener('click', ()=>
-{
-        if(ref_num){
-            document.getElementById('discolored_id').classList.add('discolored_box');
-        }
-        else{
-            document.getElementById('discolored_id').classList.remove("discolored_box");
-        }
-    ref_num = !ref_num ;    
+document.getElementById('change_bg_btn').addEventListener('click', () => {
+    if (ref_num) {
+        document.getElementById('discolored_id').classList.add('discolored_box');
+    }
+    else {
+        document.getElementById('discolored_id').classList.remove("discolored_box");
+    }
+    ref_num = !ref_num;
 }
 );
 
